9c58c2b2804120c5110d27ba5677dff2f0ec9f55
SupremeDeity <sheikhmohsin706@gmail.com>
2020-05-13 21:44:18 +0500

    Integrated Monoco

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,15 +1,32 @@
 const {ipcRenderer, dialog} = require('electron')
 const fs = require('fs')
+const loader = require('monaco-loader')
+
+let g_editor
+let g_monaco
 
 ipcRenderer.on('open', (event, args) => {
     fs.readFile(args['filepath'], (err, data) => {
         if(err) throw err
-        document.getElementById('content').innerText = data.toString()
+        let extension = args['filepath'].split('.').pop();
+        const model = g_monaco.editor.createModel(
+            data.toString(),
+            undefined, // language
+            g_monaco.Uri.file(args['filepath'])
+          )
+          
+          g_editor.setModel(model)
     })
 })
 
 ipcRenderer.on('save', (event, args) => {
-    let data = document.getElementById('content').innerText
+    const model = g_editor.getModel();
+    let data = '';
+
+    for(let x = 1; x <= model.getLineCount(); x++) {
+        data += model.getLineContent(x) + model.getEOL()
+    }
+
     fs.writeFile(args['filepath'], data, (err) => {
         if(err) throw err;
         if(!args['new']) {
@@ -18,8 +35,12 @@ ipcRenderer.on('save', (event, args) => {
     })
 })
 
-document.getElementById('content').addEventListener('paste', (event) => {
-    event.preventDefault()
-    var text = event.clipboardData.getData('text/plain')
-    document.execCommand('insertText', false, text)
+console.log('Load')
+loader().then((monaco) => {
+    g_editor = monaco.editor.create(document.getElementById('container'), {
+        language: "plaintext",
+        theme: 'vs-dark',
+        automaticLayout: true
+    })
+    g_monaco = monaco
 })
